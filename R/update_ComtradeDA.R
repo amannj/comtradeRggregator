55ba8d22c7e13de2c93c41382b0f95c55c4fe9b0
Juergen Amann <amann.juergen@gmail.com>
2021-04-25 01:42:35 +0200

    style return messages

--- R/update_ComtradeDA.R
+++ R/update_ComtradeDA.R
@@ -28,7 +28,9 @@ update_ComtradeDA <- function(directory = "data/Comtrade_DataAvailability",
       readr::write_csv(paste0(directory, "/", file, ".csv.gz"))
 
     message(paste0("\nComtrade Data Availability file updated and stored in file '", file, "' in folder '", directory, "'; will be used for look-up. Old files removed.\n"))
+    message('')
   } else if (!identical(file_exists, character(0))) {
     message(paste0("\nComtrade Data Availability file '", file, "'\n   in folder '", directory, "'\n   already exists and will be used for look-up.\n"))
+    message('')
   }
 }
