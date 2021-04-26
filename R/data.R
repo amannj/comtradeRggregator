c30a3d31fc12316df6335b99bce52306048da4d4
Juergen Amann <amann.juergen@gmail.com>
2021-04-26 16:33:23 +0200

    update

--- R/data.R
+++ R/data.R
@@ -1,34 +1,3 @@
-#' Comtrade Data Availability Table
-#'
-#' Data availability varies by country and classification, and new trade data is released/revised very frequently
-#' on [Comtrade](https://comtrade.un.org/). `comtradeRggregator` uses trade information corresponding to
-#' [official Comtrade data availability](https://comtrade.un.org/data/da) which it updates once a day and stores
-#' locally on your machine:
-#'   `<your local package location>/data/Comtrade_DataAvailability/Comtrade_DataAvailability-<date stamp>.csv.gz`
-#'   The locally stored Comtrade Data Availability file is used in every `download_Comtrade()` data query. In other
-#'   words, if new data becomes available and is documented in the
-#'    [official Comtrade data availability file](https://comtrade.un.org/data/da), `comtradeRggregator` will
-#'    use this information no later than 24 hours after its publication.
-#'
-#' @format A data frame with approx. 66k rows and 10 variables:
-#' \describe{
-#'   \item{type}{Corresponds to `type` argument in `download_Comtrade()`; either `commodities` or `services`.}
-#'   \item{freq}{Corresponds to `frequency` argument in `download_Comtrade()`; either `annual` or `monthly`.}
-#'   \item{px}{Corresponds to abbreviation of `tradecode` argument in `download_Comtrade()`.}
-#'   \item{r}{Country code.}
-#'   \item{rDesc}{Country description.}
-#'   \item{ps}{Corresponds to `year` (or `year`- `month` for monthly data) argument in `download_Comtrade()`.}
-#'   \item{TotalRecords}{Number of total recods.}
-#'   \item{isOriginal}{Is original; dummy variable.}
-#'   \item{publicationDate}{Date of publication}
-#'   \item{isPartnerDetail}{Is original; dummy variable.}
-#' }
-#' @source \url{https://comtrade.un.org/data/da}
-"Comrade_DA"
-
-
-
-
 #' HS3 to ISIC Rev. 3 Concordance Table
 #'
 #' Concordance table between `HS3` and `ISIC Rev. 3`
