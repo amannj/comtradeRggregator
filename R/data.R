50293db1ae5f3110a9b07c209fb675d699e4f76d
Juergen Amann <amann.juergen@gmail.com>
2021-04-26 14:33:37 +0200

    add data documentation

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' HS3 to ISIC Rev. 3 Concordance Table
+#'
+#' Concordance table between `HS3` and `ISIC Rev. 3`
+#' diamonds.
+#'
+#' @format A data frame with 5,052 rows and 4 variables:
+#' \describe{
+#'   \item{HS 2007 Product Code}{...}
+#'   \item{HS 2007 Product Description}{...}
+#'   \item{ISIC Revision 3 Product Code}{...}
+#'   \item{ISIC Revision 3 Product Description}{...}
+#' }
+#' @source \url{https://wits.worldbank.org/product_concordance.html}
+"H3_I3"
