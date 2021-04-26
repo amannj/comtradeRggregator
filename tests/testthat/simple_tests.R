324f2663c9cff5e545c2f0f4b02f44290e217efc
Juergen Amann <amann.juergen@gmail.com>
2021-04-26 20:59:59 +0200

    clean simple_tests.R

--- tests/testthat/simple_tests.R
+++ tests/testthat/simple_tests.R
@@ -5,13 +5,11 @@
 #                          auth_token = "..."
 # )
 library(comtradeRggregator)
-library(comtradr)
-library(tidyverse)
 
 ## Please update to use your own when testing
-#mytoken <- "lhsmuyUVrMuKuxtet0TbbJdfVUChniTF2DqR8kUxb9PqNxf2aQ+AqE+BwKHKycyg84HWaXUszKnPCKJ8zO5eFqoANzWiPaGX7dX/XEUAO0jImhk6ro/YqiL4hsPn4VLxHK5hSnE8vsHebKzR28GjyA=="
-#mytempDirectory <- "C:/Users/juerg/Desktop/tmp"
-
+mytoken <- "lhsmuyUVrMuKuxtet0TbbJdfVUChniTF2DqR8kUxb9PqNxf2aQ+AqE+BwKHKycyg84HWaXUszKnPCKJ8zO5eFqoANzWiPaGX7dX/XEUAO0jImhk6ro/YqiL4hsPn4VLxHK5hSnE8vsHebKzR28GjyA=="
+mytempDirectory <- "C:/Users/juerg/Desktop/tmp"
+#mytoken <- '...'
 
 # Test 1: Basic country selection and storage of tmp files  -------------------------------
 ### Works for Austria
