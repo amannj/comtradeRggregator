1447cc317c4c2c483fefd3f901d2fdd22a4f5e5d
Juergen Amann <amann.juergen@gmail.com>
2021-04-29 13:51:32 +0200

    add package documentation

---
+++ R/comtradeRggregator.R
@@ -0,0 +1,42 @@
+#' \code{comtradeRggregator} package
+#'
+#' The goal of the `comtradeRggregator` package is to provide a simple tool to
+#' download and reclassify large chunks of [Comtrade trade data](https://comtrade.un.org).
+#' It offers functionality to download and aggregate [Comtrade trade data](https://comtrade.un.org)
+#' using [Comtrade's API](https://comtrade.un.org/data/doc/api/) by building on
+#' the [comtradr](https://github.com/ropensci/comtradr) package. Furthermore, it provides a
+#' function for easy reclassification of trade data using official concordance tables from
+#' the [United Nations Statistical Division (UNSD)](https://unstats.un.org/unsd/trade/classifications/correspondence-tables.asp)
+#' and the [World Integrated Trade Solution (WITS)](https://wits.worldbank.org/product_concordance.html).
+#'
+#' See the README on
+#'  \href{https://github.com/amannj/comtradeRggregator}{GitHub}.
+#'
+#' @docType package
+#' @name comtradeRggregator
+NULL
+
+## quiets concerns of R CMD check
+if (getRversion() >= "2.15.1") {
+  utils::globalVariables(c(
+    ".",
+    "lz",
+    "loc_volder",
+    "reporter",
+    "trade_flow",
+    "classifcation",
+    "period",
+    "partnernew",
+    "commodity_code",
+    "commodity",
+    ".select.stats",
+    "classification",
+    "period",
+    "trade_flow",
+    ".px",
+    "freq",
+    "rDesc",
+    "ps",
+    "H0_BE"
+  ))
+}
