94b8ac908de01895037e6ff12db1ef6a280ee737
Juergen Amann <amann.juergen@gmail.com>
2021-05-13 16:45:22 +0200

    update test

--- tests/testthat/test-rm_temporaryFiles.R
+++ tests/testthat/test-rm_temporaryFiles.R
@@ -3,22 +3,33 @@ library(testthat) # load testthat package
 local_edition(3)
 library(comtradeRggregator) # load package
 
-test_that("remove temporary files of folder that exists", {
+test_that("remove temporary files of folder that exists but file does not exist", {
   rm1 <- function() rm_temporaryFiles(location.temporaryFiles = NULL)
 
   expect_message(rm1(), "Temporary*")
 })
 
+test_that("remove temporary files of folder that exists and file exists", {
 
-test_that("remove temporary files of folder that does not exist (wrong class)", {
+  df = tibble(a = c(1,2,3))
+  loc = 'C:/Users/juerg/Desktop/tmp'
+  saveRDS(df, paste0(loc,'/df.rds'))
+
+  rm1 <- function() rm_temporaryFiles(location.temporaryFiles = loc)
+
+  expect_message(rm1(), "Temporary*")
+})
+
+test_that("try to remove temporary files of folder that does not exist (wrong class)", {
   rm1 <- function() rm_temporaryFiles(location.temporaryFiles = 12)
 
   expect_error(rm1(), "Location for temporary files defined incorrectly.*")
 })
 
 
-test_that("remove temporary files of folder that does not exist (wrong location)", {
+test_that("try to remove temporary files of folder that does not exist (wrong location)", {
   rm1 <- function() rm_temporaryFiles(location.temporaryFiles = "random location")
 
   expect_error(rm1(), "Location for temporary files defined incorrectly.*")
 })
+
