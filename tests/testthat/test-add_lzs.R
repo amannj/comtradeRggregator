f7ed96c63a128abf901f3067e16f302018b8f082
Juergen Amann <amann.juergen@gmail.com>
2021-05-14 16:09:08 +0200

    add gen_lzs() test

--- tests/testthat/test-add_lzs.R
+++ tests/testthat/test-add_lzs.R
@@ -8,8 +8,13 @@ test_that("Add_ldz tests", {
   data <- add_lzs(data = df, variable = "var", variable.length = 3)
   expect_s3_class(data, "data.frame")
   expect_equal(nrow(data), 3)
-  expect_equal(data %>% pull() %>% .[1], '001')
-  expect_equal(data %>% pull() %>% .[2], '011')
-  expect_equal(data %>% pull() %>% .[3], '111')
+  expect_equal(data %>% pull() %>% .[1], "001")
+  expect_equal(data %>% pull() %>% .[2], "011")
+  expect_equal(data %>% pull() %>% .[3], "111")
 })
 
+
+test_that("gen_lzs() needs numeric input", {
+  r1 <- function() gen_lzs('1')
+  expect_error(r1(), "Input*")
+})
