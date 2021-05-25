3abcafd3ded3845af16ab441efdc8d206b0f6f8f
Juergen Amann <amann.juergen@gmail.com>
2021-05-25 13:17:44 +0200

    remove example

--- R/rm_temporaryFiles.R
+++ R/rm_temporaryFiles.R
@@ -22,9 +22,6 @@
 #' @import dplyr comtradr tibble readr rlang
 #' @examples
 #' rm_temporaryFiles(location.temporaryFiles = NULL)
-#' \dontrun{
-#' rm_temporaryFiles(location.temporaryFiles = "<your-temporary-download-directory>")
-#' }
 rm_temporaryFiles <- function(location.temporaryFiles = NULL) {
 
   ## Obtain location of temporary directory
