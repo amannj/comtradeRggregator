a5b14f9b52a6436a83e9d219762614dd3bcb6dec
Juergen Amann <amann.juergen@gmail.com>
2021-06-21 20:29:39 +0200

    Fix Comtrade API bug for Eswatini: https://github.com/ropensci/comtradr/issues/29

--- R/download_Comtrade.R
+++ R/download_Comtrade.R
@@ -94,11 +94,14 @@ download_Comtrade <- function(year = "2018", #  Years for which to extract
 
 
   ## Check `countries` ------
-  ### see data extraction
-
+  ## Fix Comtrade API bug for Eswatini: https://github.com/ropensci/comtradr/issues/29
+  countries[countries %in% "Eswatini"] = 'Swaziland'
+  countries <- unique(countries)
 
   ## Check `partners` ------
-  ### see data extraction
+  ## Fix Comtrade API bug for Eswatini: https://github.com/ropensci/comtradr/issues/29
+  partners[partners %in% "Eswatini"] = 'Swaziland'
+  partners <- unique(partners)
 
 
   ## Check `tradecode` and return arg  ------
