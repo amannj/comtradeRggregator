84abd00aabe8a5192a3471f3ead6510b2fd219b6
amannj <amann.juergen@gmail.com>
2022-05-31 23:58:05 +0100

    update

--- R/comtradeRggregator-package.R
+++ R/comtradeRggregator-package.R
@@ -1,4 +1,9 @@
-#' @keywords internal
+#' comtradeRggregator package
+#'
+#' @name comtradeRggregator
+#' @docType package
+#' @aliases comtradeRggregator-package
+#' #' @keywords internal
 "_PACKAGE"
 
 ## usethis namespace: start
